878173f18aeebfe954ca93c91946adf7c1bd0891
Oleksandr Martsenkov <omartsenkov.appwell@ancestry.com>
2023-11-23 11:24:16 +0200

    Add sweettv page parser

---
+++ src/test/java/com/example/seriesbackend/SeriesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.seriesbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeriesBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
